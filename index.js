722321c57ceba678ae30b545e01962580b9f35a3
Alka sah <alkasah485@gmail.com>
2022-08-05 17:25:00 -0700

    link to team from lib in index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // link to page creation
 const PageTemplate = require('./src/Pagetemplate');
+
+//team profiles
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
