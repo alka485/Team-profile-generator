e2e44989889e86e9fb1d2799c9cd24de54e9f395
Alka sah <alkasah485@gmail.com>
2022-08-05 17:35:47 -0700

    added inquirer and fs in index file

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 const PageTemplate = require('./src/Pagetemplate');
 
 //team profiles
-
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+//node modules
+const inquirer = require('inquirer');
+const fs = require('fs');
