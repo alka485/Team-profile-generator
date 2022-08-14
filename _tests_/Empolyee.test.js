42516dec1aca0540b6f2ba21741af9cbc698897c
Alka sah <alkasah485@gmail.com>
2022-08-13 23:49:22 -0700

    updation in employee test.js

--- _tests_/Empolyee.test.js
+++ _tests_/Empolyee.test.js
@@ -6,3 +6,17 @@ test('creates an employee object',() => {
     expect(employee.id)  .toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 });
+
+test('gets employee name', () => {
+    const employee = new Employee('Alka',23,'alkasah485@gmail.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test('gets employee email', () =>
+ {
+    const employee = new Employee('Alka',23,'alkasah485@gmail.com');
+
+    expect(employee.getEmail()).toEqual(expect.StringContaining(employee.email.toString()));
+});
+
