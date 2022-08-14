103373a80e379130209fecec31d99310f13468c1
Alka sah <alkasah485@gmail.com>
2022-08-14 16:54:03 -0700

    updation in engineer test file

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -5,3 +5,17 @@ test('creates an engineer object',() => {
     expect(engineer.github).toEqual(expect.any(String));
     
 });
+
+test('gets engineer github value', () =>
+ {
+    const engineer = new Engineer('Alka',23,'alkasah485@gmail.com','alka485');
+
+    expect(engineer.getGithub()).toEqual(expect.StringContaining(engineer.github.toString()));
+});
+
+test('gets role of employee', () => {
+    const employee = new Employee('Alka',23,'alkasah485@gmail.com');
+    expect(employee.getRole()).toEqual("Engineer");
+});
+
+
